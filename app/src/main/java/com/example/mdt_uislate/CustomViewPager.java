4ce6decccb986e5c3b1183596c1c23483764d489
Tajveer Nijjar <Tajveer.Nijjar@connexionz.com>
2021-03-11 13:57:11 +1300

    Not initial commit.

---
+++ app/src/main/java/com/example/mdt_uislate/CustomViewPager.java
@@ -0,0 +1,12 @@
+package com.example.mdt_uislate;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.ViewPager;
+
+public class CustomViewPager extends ViewPager {
+    public CustomViewPager(@NonNull Context context) {
+        super(context);
+    }
+}
